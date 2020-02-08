17b51a8aac6b3926d414e032869186d898f9a911
mic19 <kielski.michal@gmail.com>
2020-02-08 17:11:55 +0100

    Basic functionality. Dijkstra algorithm visualized step by step.

---
+++ state.py
@@ -0,0 +1,26 @@
+from abc import ABC
+
+
+# State design pattern
+class State(ABC):
+	def __init__(self):
+		pass
+
+	def handle(self):
+		pass
+
+
+class SelectState(State):
+	def __init__(self):
+		pass
+
+	def handle(self):
+		pass
+
+
+class StartState(State):
+	def __init__(self):
+		pass
+
+	def handle(self):
+		pass
